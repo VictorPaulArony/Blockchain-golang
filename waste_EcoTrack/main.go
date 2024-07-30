d0f557fb6f26af1f3d1337ead22118bfa21086cf
victorpaularony <smallypauls@gmail.com>
2024-07-30 15:00:17 +0300

    FILE: addition of methods

--- waste_EcoTrack/main.go
+++ waste_EcoTrack/main.go
@@ -16,6 +16,7 @@ func main() {
 	http.HandleFunc("/resident-login", handlers.ResidentLoginHandler)
 	http.HandleFunc("/staff-register", handlers.StaffRegistrationHandler)
 	http.HandleFunc("/staff-login", handlers.StaffLoginHandler)
+	http.HandleFunc("/company-dashboard", handlers.StaffDshboardHandler)
 	//http.HandleFunc("/resident-dash", handlers.)
 
 	log.Println("server running at : http://localhost:1234")
