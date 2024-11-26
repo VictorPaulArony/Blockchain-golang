13981a25a4a4ec69351c54bd156ae0e9d6e68f7f
viarony <smallypauls@gmail.com>
2024-11-26 08:57:23 +0300

    all corrections

--- goroutine/main.go
+++ goroutine/main.go
@@ -3,114 +3,46 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net"
 	"os"
-	"strconv"
-	"strings"
 )
 
-type Account struct {
-	Balance float64
-}
-
 func main() {
-	account := &Account{Balance: 120.00}
+	server, _ := net.Listen("tcp", ":1234")
 
-	input := bufio.NewScanner(os.Stdin)
-
+	defer server.Close()
+	fmt.Println("server started at port 1234")
 	for {
+		conn, _ := server.Accept()
 
-		fmt.Println("\n ------- Welcome to your account-----")
-		fmt.Println("1. Check Balance")
-		fmt.Println("2. Withdraw cash")
-		fmt.Println("3. Deposite Cash")
-		fmt.Println("4. Exit")
-		fmt.Println("choose an option")
+		fmt.Println("Client Connected at: ", conn.RemoteAddr())
 
-		input.Scan()
+		go ConnectionHandler(conn)
+	}
+}
 
-		options := strings.TrimSpace(input.Text())
+// function for the client to dial from
+func ConnectionHandler(conn net.Conn) {
+	defer conn.Close()
 
-		switch options {
-		case "1":
-			account.CheckBalance()
-		case "2":
-			fmt.Println("Enter amount to withdraw")
-			input.Scan()
-			amount, _ := strconv.ParseFloat(strings.TrimSpace(input.Text()), 64)
-			account.Withdraw(amount)
-		case "3":
-			fmt.Println("Enter amaount to Deposite")
-			input.Scan()
-			amount, _ := strconv.ParseFloat(strings.TrimSpace(input.Text()), 64)
-			account.Deposite(amount)
-		case "4":
-			fmt.Println("Thank you, come back again")
-			return
-		default:
-			fmt.Println("Invalid option, Try again")
+	//handle outging sms
+	go func() {
+		input := bufio.NewScanner(os.Stdin)
+		for input.Scan() {
+			sms := input.Text() + "\n"
+			conn.Write([]byte( sms))
 		}
-
+	}()
+		
+	//handle iincomming sms
+	for {
+		
+		response, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println("Client disconnected")
+			return
+		}
+		fmt.Println(response)
+		fmt.Print("Server: ")
 	}
-}
-
-// function main using fmt.Scanln functionality
-// func main() {
-// 	account := &Account{Balance: 120}
-
-// 	for {
-// 		fmt.Println("\n ------- Welcome to your account-----")
-// 		fmt.Println("1. Check Balance")
-// 		fmt.Println("2. Withdraw cash")
-// 		fmt.Println("3. Deposite Cash")
-// 		fmt.Println("4. Exit")
-// 		fmt.Println("choose an option")
-
-// 		var option string
-
-// 		fmt.Scanln(&option)
-// 		switch option {
-// 		case "1":
-// 			fmt.Println()
-// 			account.CheckBalance()
-// 		case "2":
-// 			fmt.Println("Enter amount to withdraw")
-// 			var amount string
-// 			fmt.Scanln(&amount)
-// 			am, _ := strconv.ParseFloat(amount, 64)
-// 			account.Withdraw(am)
-// 		case "3":
-// 			fmt.Println("Enter amaount to Deposite")
-// 			var amount string
-// 			fmt.Scanln(&amount)
-// 			am, _ := strconv.ParseFloat(amount, 64)
-// 			account.Deposite(am)
-// 		case "4":
-// 			fmt.Println("Thank you, come back again")
-// 			return
-// 		default:
-// 			fmt.Println("Invalid option, Try again")
-
-// 		}
-// 	}
-// }
-
-// function to check the balance in the account
-func (acc *Account) CheckBalance() {
-	fmt.Printf("Your Balance: %v\n", acc.Balance)
-}
-
-// function to withdraw amount from the account
-func (acc *Account) Withdraw(amount float64) {
-	if amount > acc.Balance {
-		fmt.Printf("Insufficient account balance, your account balance is %v\n", acc.Balance)
-		return
-	}
-	acc.Balance -= amount
-	fmt.Printf("Successfully withdrew: %.2f\n", amount)
-}
-
-// function to Deposite the amount in the account
-func (acc *Account) Deposite(amount float64) {
-	acc.Balance += amount
-	fmt.Printf("Successfully Deposited: %.2f\n", amount)
 }
--- practise/p2p.go
+++ practise/p2p.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
 	"log"
-	"math/rand"
+	"math/big"
 	"strconv"
 	"sync"
 	"time"
 )
+
+type Transaction struct {
+	ID        int
+	Sender    string
+	Receiver  string
+	TimeStamp string
+	Amount    float64
+	Signature string
+}
+
+type Mempool struct {
+	Transaction []Transaction
+	mu          sync.Mutex
+}
 
 type Block struct {
 	ID          int
@@ -21,19 +38,22 @@ type Block struct {
 	Nonce       int
 }
 
-type Transaction struct {
-	Sender   string
-	Receiver string
-	Amount   float64
-}
-
 type Blockchain struct {
 	Blocks []Block
 	mu     sync.Mutex
 }
 
-// Function to generate the a salt
-//(to prevent collision attack to the blocks during hashing)
+type Address struct {
+	PrivateKey *ecdsa.PrivateKey
+	PublicKey  *ecdsa.PublicKey
+	Balance    float64
+}
+
+type Wallet struct {
+	Address map[string]*Address
+}
+
+// function to generate random value for the salt
 func Salt() (string, error) {
 	salt := make([]byte, 16)
 	if _, err := rand.Read(salt); err != nil {
@@ -42,24 +62,195 @@ func Salt() (string, error) {
 	return hex.EncodeToString(salt), nil
 }
 
-// fuction to create do the hashing
-func Hash(data string, salt string) string {
+// function to generate the hashing functions
+func Hash(data, salt string) string {
 	hash := sha256.Sum256([]byte(data + salt))
 	return hex.EncodeToString(hash[:])
 }
 
-// function to hash the transaction block
-func (b *Block) HashBlock() string {
-	data := strconv.Itoa(b.ID) + b.TimeStamp + b.MerkleRoot + b.PrevHash + b.Hash + strconv.Itoa(b.Nonce)
-	for _, tx := range b.Transaction {
-		data += tx.Sender + tx.Receiver + strconv.Itoa(int(tx.Amount))
-	}
-	salt, _ := Salt()
-	return Hash(data, salt)
+// function to create the wallate
+func CreateWallet() Wallet {
+	return Wallet{make(map[string]*Address)}
 }
 
-// function to create the genesis block of thwe blockchain
-func GenesisBlock() Block {
+// function to create the private and public key of the address
+func (w *Wallet) CreateAddress() string {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		log.Println(err)
+	}
+
+	publicKey := privateKey.PublicKey
+	publicKeyBytes := append(publicKey.X.Bytes(), publicKey.Y.Bytes()...)
+	AdrHash := sha256.Sum256(publicKeyBytes)
+	address := hex.EncodeToString(AdrHash[:])
+
+	w.Address[address] = &Address{
+		PrivateKey: privateKey,
+		PublicKey:  &publicKey,
+		Balance:    0,
+	}
+	return address
+}
+
+// function to create a mempool for the transaction
+func CreateMempool() Mempool {
+	return Mempool{Transaction: []Transaction{}}
+}
+
+// function create a transaction
+func (w *Wallet) CreateTransaction(from, to string, amount float64) error {
+	mp := &Mempool{}
+	mp.mu.Lock()
+	defer mp.mu.Unlock()
+
+	sender, exist := w.Address[from]
+	if !exist {
+		log.Fatalln(from)
+	}
+
+	receiver, exist := w.Address[to]
+	if !exist {
+		log.Fatalln(to)
+	}
+
+	if w.Address[from].Balance < amount {
+		log.Println("Insufficient Funds In the Wallet")
+		return fmt.Errorf("insufficient funds")
+	}
+
+	// Check if there are previous transactions
+	var prevTxID int
+	if len(mp.Transaction) > 0 {
+		prevTx := mp.Transaction[len(mp.Transaction)-1]
+		prevTxID = prevTx.ID
+	} else {
+		prevTxID = 0
+	}
+
+	transaction := &Transaction{
+		ID:        prevTxID + 1,
+		Sender:    from,
+		Receiver:  to,
+		TimeStamp: time.Now().String(),
+		Amount:    amount,
+	}
+
+	err := w.SignTransaction(transaction)
+	if err != nil {
+		log.Println(err)
+	}
+
+	if !w.IsValidTransaction(transaction) {
+		log.Println("Invalid Transaction")
+	}
+
+	sender.Balance -= amount
+	receiver.Balance += amount
+
+	mp.Transaction = append(mp.Transaction, *transaction)
+	return nil
+}
+
+// Fuunction to sign the transaction
+func (w *Wallet) SignTransaction(tx *Transaction) error {
+	addr, exist := w.Address[tx.Sender]
+	if !exist {
+		log.Fatalln(tx.Sender)
+	}
+
+	salt, err := Salt()
+	if err != nil {
+		log.Println(err)
+	}
+
+	hash := Hash((strconv.Itoa(tx.ID) + tx.Sender + tx.Receiver + tx.TimeStamp + strconv.Itoa(int(tx.Amount))), salt)
+	r, s, err := ecdsa.Sign(rand.Reader, addr.PrivateKey, []byte(hash))
+	if err != nil {
+		log.Println(err)
+	}
+
+	signature := append(r.Bytes(), s.Bytes()...)
+
+	tx.Signature = hex.EncodeToString(signature)
+
+	return nil
+}
+
+// function to verify the transaction
+func (w *Wallet) IsValidTransaction(tx *Transaction) bool {
+	addr, exist := w.Address[tx.Sender]
+	if !exist {
+		log.Println(tx.Sender)
+	}
+
+	salt, err := Salt()
+	if err != nil {
+		log.Println(err)
+	}
+	hash := Hash(strconv.Itoa(tx.ID)+tx.Receiver+tx.Sender+tx.TimeStamp+strconv.Itoa(int(tx.Amount)), salt)
+	signature, err := hex.DecodeString(tx.Signature)
+	if err != nil {
+		log.Println(err)
+	}
+
+	r := big.Int{}
+	s := big.Int{}
+
+	signLen := len(signature)
+
+	r.SetBytes(signature[:(signLen / 2)])
+	s.SetBytes(signature[(signLen / 2):])
+	return ecdsa.Verify(&addr.PrivateKey.PublicKey, []byte(hash[:]), &r, &s)
+}
+
+// fnction to form the merkel tree root for a transaction
+func MerkleRoots(transaction []Transaction) string {
+	if len(transaction) == 0 {
+		return ""
+	}
+
+	var hashes []string
+
+	for _, tx := range transaction {
+		salt, err := Salt()
+		if err != nil {
+			log.Println(err)
+		}
+		hash := Hash(strconv.Itoa(tx.ID)+tx.Receiver+tx.Sender+tx.TimeStamp+strconv.Itoa(int(tx.Amount)), salt)
+		hashes = append(hashes, hash)
+	}
+
+	for len(hashes) > 1 {
+		var newHashes []string
+		for i := 0; i < len(hashes); i += 2 {
+			if i+1 < len(hashes) {
+				newHashes = append(newHashes, Hash(hashes[i]+hashes[i+1], "")) // Fixed hashing logic for combining pairs
+			} else {
+				newHashes = append(newHashes, hashes[i])
+			}
+		}
+		hashes = newHashes
+	}
+	return hashes[0]
+}
+
+// functon to create a hashing function fo the block
+func (b *Block) CreateHash() string {
+	salt, err := Salt()
+	if err != nil {
+		log.Println(err)
+	}
+
+	hash := strconv.Itoa(b.ID) + b.MerkleRoot + b.TimeStamp + b.PrevHash + b.Hash + strconv.Itoa(b.Nonce)
+	// var tx *Transaction
+	// hash += strconv.Itoa(tx.ID) + tx.Receiver + tx.Sender + tx.Signature + tx.TimeStamp + strconv.Itoa(int(tx.Amount))
+
+	return Hash(hash, salt)
+}
+
+// function to create a genesis block of the blockchain
+func CreateGenesis() Block {
 	var transaction []Transaction
 	genesis := Block{
 		ID:          0,
@@ -67,43 +258,46 @@ func GenesisBlock() Block {
 		TimeStamp:   time.Now().String(),
 		PrevHash:    "",
 	}
+
 	for {
-		genesis.Hash = genesis.HashBlock()
-		if IsValidHash(genesis.Hash) {
+		genesis.Hash = genesis.CreateHash()
+		if !IsValidHash(genesis.Hash) {
 			break
 		}
 		genesis.Nonce++
 	}
+
 	return genesis
 }
 
-// function to verify the nonce of the block (PoW)
+// function to create the concensus ie. PoW
 func IsValidHash(hash string) bool {
 	return hash[:5] == "00000"
 }
 
-// function to create a new blockchain
+// function to craeta a new blockchain
 func CreateBlockchain() Blockchain {
-	genesis := GenesisBlock()
+	genesis := CreateGenesis()
 	return Blockchain{Blocks: []Block{genesis}}
 }
 
-// Function to verify the blocks before adding to the blockchain
-func (bc *Blockchain) IsValidBlock(block Block) bool {
-	if len(block.Hash) == 0 {
-		return false
-	}
-	prevBlock := bc.Blocks[len(bc.Blocks)-1]
-	if prevBlock.Hash == block.PrevHash {
-		return true
-	}
-	return false
-}
-
-// function to Add the block to the blockchain
-func (bc *Blockchain) AddBlock(transaction []Transaction) {
+// function to create and add blocks to the blockchain
+func (bc *Blockchain) AddBlock() {
 	bc.mu.Lock()
 	defer bc.mu.Unlock()
+
+	var mp *Mempool
+	mp = &Mempool{}
+
+	mp.mu.Lock()
+	defer mp.mu.Unlock()
+
+	// retrieve transaction from mempool
+	transaction := mp.Transaction
+	if len(transaction) == 0 {
+		log.Println("N Transaction to Mine")
+		return
+	}
 
 	prevBlock := bc.Blocks[len(bc.Blocks)-1]
 	newBlock := Block{
@@ -113,75 +307,76 @@ func (bc *Blockchain) AddBlock(transaction []Transaction) {
 		PrevHash:    prevBlock.Hash,
 	}
 
-	newBlock.MerkleRoot = CreateMerkelTree(transaction)
+	newBlock.MerkleRoot = MerkleRoots(transaction)
+
 	for {
-		newBlock.Hash = newBlock.HashBlock()
-		if IsValidHash(newBlock.Hash) {
+		newBlock.Hash = newBlock.CreateHash()
+		if !IsValidHash(newBlock.Hash) {
 			break
 		}
 		newBlock.Nonce++
 	}
-
 	if !bc.IsValidBlock(newBlock) {
-		log.Fatalln("The Block is an Invalid Block")
+		log.Println("Invalid Block")
 		return
 	}
 	bc.Blocks = append(bc.Blocks, newBlock)
+
+	// clear mempool for next transaction block
+	mp.Transaction = []Transaction{}
 }
 
-// function to calculate the merkle root from a slice of transaction
-func CreateMerkelTree(transaction []Transaction) string {
-	var hashes []string
-
-	if len(transaction) == 0 {
-		return ""
-	}
-	salt, _ := Salt()
-	for _, tx := range transaction {
-		hashes = append(hashes, Hash(tx.Receiver+tx.Sender+strconv.Itoa(int(tx.Amount)), salt))
+// function to validate the blocks before adding to the blockchain
+func (bc *Blockchain) IsValidBlock(block Block) bool {
+	if len(block.Hash) == 0 {
+		return false
 	}
 
-	for len(hashes) > 1 {
-		var newHash []string
-		for i := 0; i < len(hashes); i += 2 {
-			if i+1 < len(hashes) {
-				newHash = append(newHash, Hash(hashes[i]+hashes[i+1], salt))
-			} else {
-				newHash = append(newHash, hashes[i])
-			}
-		}
-		hashes = newHash
-	}
+	prevBlock := bc.Blocks[len(bc.Blocks)-1]
 
-	return hashes[0]
+	if prevBlock.Hash == block.PrevHash {
+		return true
+	}
+	return false
 }
 
-// function to print the output on the terminal of the CLI
-func (bc *Blockchain) Display() {
-	for _, block := range bc.Blocks {
-		fmt.Printf("Block ID: %d\n", block.ID)
-		fmt.Printf("  MerkleRoot: %s\n", block.MerkleRoot)
-		fmt.Printf("  PrevHash: %s\n", block.PrevHash)
-		fmt.Printf("  Hash: %s\n", block.Hash)
-		fmt.Printf("  Nonce: %d\n", block.Nonce)
-		for _, tx := range block.Transaction {
-			fmt.Printf("Sender %s to Receiver %s amount %.f\n", tx.Receiver, tx.Sender, tx.Amount)
-		}
-	}
-}
-
-// function main to run the code on CLI mode
+// main function to run the project
 func main() {
+	wallet := CreateWallet()
+
 	blockchain := CreateBlockchain()
 
-	blockchain.AddBlock(
-		[]Transaction{
-			{
-				Sender:   "paul",
-				Receiver: "Smally",
-				Amount:   150.0,
-			},
-		},
-	)
-	blockchain.Display()
+	address1 := wallet.CreateAddress()
+	address2 := wallet.CreateAddress()
+
+	fmt.Printf("Address 1: %s\n", address1)
+	fmt.Printf("Address 2: %s\n", address2)
+
+	wallet.Address[address1].Balance = 100
+
+	fmt.Printf("Initial Balance of Address 1: %.2f\n", wallet.Address[address1].Balance)
+	fmt.Printf("Initial Balance of Address 2: %.2f\n", wallet.Address[address2].Balance)
+
+	err := wallet.CreateTransaction(address1, address2, 50)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	blockchain.AddBlock()
+
+	fmt.Printf("Balance of Address 1 after transfer: %.2f\n", wallet.Address[address1].Balance)
+	fmt.Printf("Balance of Address 2 after transfer: %.2f\n", wallet.Address[address2].Balance)
+
+	// Print the blockchain
+	for _, block := range blockchain.Blocks {
+		fmt.Printf("Block ID: %d\n", block.ID)
+		fmt.Printf("Timestamp: %s\n", block.TimeStamp)
+		fmt.Printf("Previous Hash: %s\n", block.PrevHash)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Transactions:\n")
+		for _, tx := range block.Transaction {
+			fmt.Printf("  %s -> %s: %f\n", tx.Sender, tx.Receiver, tx.Amount)
+		}
+		fmt.Println()
+	}
 }
