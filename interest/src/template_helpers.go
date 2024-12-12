22f9b336c80f71519833c0e801d6a12c992512ea
viarony <smallypauls@gmail.com>
2024-12-12 14:43:11 +0300

    src/template_helpers.go

--- interest/src/template_helpers.go
+++ interest/src/template_helpers.go
@@ -6,3 +6,7 @@ import "time"
 func UnixToTime(unixTime int64) string {
 	return time.Unix(unixTime, 0).Format("2006-01-02 15:04:05")
 }
+
+func Mul(a, b float64) float64 { // New multiplication function
+	return a * b
+}
